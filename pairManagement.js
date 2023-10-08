c1bb066d60e382853f60de79d4c97fa422dbde9e
matthusala <mathyasyazew@gmail.com>
2023-10-08 16:57:38 +0300

    return an array with the provided argument

--- pairManagement.js
+++ pairManagement.js
@@ -1,5 +1,5 @@
-let createArray(arg1, arg2){
-    return [arg1, arg2]
-}
-const resultArray = createArray();
+let createArray = (arg1, arg2) => {
+  return [arg1, arg2];
+};
+const resultArray = createArray("value1", 11);
 console.log(resultArray);
