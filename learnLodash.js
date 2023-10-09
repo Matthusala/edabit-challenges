fec4a950053bb441411438c06286ceac7cd2d4bf
Matthusala <88359082+Matthusala@users.noreply.github.com>
2023-10-09 15:42:16 +0300

    Update learnLodash.js

--- learnLodash.js
+++ learnLodash.js
@@ -1,10 +1,9 @@
-function drop(arr, n = 1) {
+const drop = (arr, n=1) => {
   if (arr.length === 0) {
     return [];
   }
   return arr.slice(n);
 }
-
 const myArray = [1, 2, 3, 4, 5];
 const droppedArray = drop(myArray, 2);
 console.log(droppedArray);
