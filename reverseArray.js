54d731e6d60769a62ad3fd2456f75f5ca2821e63
matthusala <mathyasyazew@gmail.com>
2023-10-10 17:12:41 +0300

    reverse

---
+++ reverseArray.js
@@ -0,0 +1,8 @@
+function reverseArray(arr) {
+  return arr.reverse();
+}
+const originalArray = [1, 2, 3, 4, 5];
+const reversedArray = reverseArray(originalArray);
+
+console.log("Original Array:", originalArray);
+console.log("Reversed Array:", reversedArray);
