d54b162d8bd72b30d554ac17f05ba93ea3595648
matthusala <mathyasyazew@gmail.com>
2023-10-11 19:50:18 +0300

    update

--- return_Last_element.js
+++ return_Last_element.js
@@ -1,9 +1,11 @@
-function getLastItem(arr) {
+const getLastItem = (arr) => {
   if (arr.length > 0) {
     return arr[arr.length - 1];
   } else {
     return "Array is empty";
   }
-}
+};
 
 console.log(getLastItem([1, 2, 3]));
+console.log(getLastItem(["cat", "dog", "duck"]));
+console.log(getLastItem([true, false, true]));
