f97c54144e4830013f2555947f04a028d9e249d2
matthusala <mathyasyazew@gmail.com>
2023-10-12 07:27:31 +0300

    test

---
+++ find_the_index.js
@@ -0,0 +1,6 @@
+function findIndex(arr, target) {
+  return arr.indexOf(target);
+}
+const array1 = ["apple", "banana", "orange", "grape"];
+const result1 = findIndex(array1, "orange");
+console.log(result1);
