2fcf710e20d152cb62747fdef74689e9d093d3fd
matthusala <mathyasyazew@gmail.com>
2023-10-12 07:30:04 +0300

    takes an array and a string as arguments and returns the index of the string

--- find_the_index.js
+++ find_the_index.js
@@ -1,6 +1,10 @@
-function findIndex(arr, target) {
+const findIndex = (arr, target) => {
   return arr.indexOf(target);
-}
+};
 const array1 = ["apple", "banana", "orange", "grape"];
 const result1 = findIndex(array1, "orange");
 console.log(result1);
+
+const array2 = ["cat", "dog", "fish", "bird"];
+const result2 = findIndex(array2, "dog");
+console.log(result2);
