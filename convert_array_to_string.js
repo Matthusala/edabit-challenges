56355d46ef77278ff4776ea24b1ddd47c9f241f2
Matthusala <88359082+Matthusala@users.noreply.github.com>
2023-10-12 07:15:32 +0300

    Update convert_array_to_string.js

--- convert_array_to_string.js
+++ convert_array_to_string.js
@@ -1,5 +1,12 @@
 const arrayToString = (arr) => {
   return arr.join("");
 };
+
 const result1 = arrayToString([1, 2, 3]);
 console.log(result1);
+
+const result2 = arrayToString(["a", "b", "c"]);
+console.log(result2);
+
+const result3 = arrayToString([1, "a", 2, "b"]);
+console.log(result3); 
