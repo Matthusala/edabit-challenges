0be864bfc7d468a3d1216c6f54555e9265dd0a05
matthusala <mathyasyazew@gmail.com>
2023-10-13 21:53:49 +0300

    less than greater than

--- logic_operators.js
+++ logic_operators.js
@@ -1,3 +1,9 @@
-function filterNumbersBetween(num1, num2, arr) {
-    return arr.filter(number => number > num1 && number < num2);
-}
+const filterNumbersBetween = (num1, num2, arr) =>
+  arr.filter((number) => number > num1 && number < num2);
+
+const myArray = [1, 5, 10, 15, 20, 25];
+const lowerLimit = 5;
+const upperLimit = 20;
+
+const resultArray = filterNumbersBetween(lowerLimit, upperLimit, myArray);
+console.log(resultArray);
