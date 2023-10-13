df2f15f53d0851d8f97cadf9f12caf07c283cd7b
matthusala <mathyasyazew@gmail.com>
2023-10-13 21:49:43 +0300

    update

--- check_particular_number.js
+++ check_particular_number.js
@@ -4,3 +4,8 @@ function containsNumber(arr, number) {
 
 const myArray = [1, 2, 3, 4, 5];
 const targetNumber = 3;
+if (containsNumber(myArray, targetNumber)) {
+  console.log(`The array contains ${targetNumber}.`);
+} else {
+  console.log(`The array does not contain ${targetNumber}.`);
+}
