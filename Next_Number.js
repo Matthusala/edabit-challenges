58aedb2a5daad84723b6b5919c80341c7615fb51
matthusala <mathyasyazew@gmail.com>
2023-10-27 16:19:23 +0300

    Update

--- Next_Number.js
+++ Next_Number.js
@@ -2,7 +2,7 @@ function incrementByOne(number) {
   return number + 1;
 }
 
-// Example usage:
+
 let originalNumber = 5;
 let result = incrementByOne(originalNumber);
-console.log(result); // Output: 6
+console.log(result); 
