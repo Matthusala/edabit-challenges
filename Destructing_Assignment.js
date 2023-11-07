ab045219d326eb3ebe2dd29fb2cfb767f870e07f
matthusala <mathyasyazew@gmail.com>
2023-11-07 23:46:58 +0300

    Test

---
+++ Destructing_Assignment.js
@@ -0,0 +1,9 @@
+const { title, price, description } = {
+  title: "iPhone",
+  price: 999,
+  description: "The iPhone is a smartphone developed by Apple",
+};
+
+console.log(title);
+console.log(price);
+console.log(description);
