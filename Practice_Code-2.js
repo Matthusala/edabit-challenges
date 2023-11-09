633dcf6dd965cab99aaf854cd6930bd81f42549d
matthusala <mathyasyazew@gmail.com>
2023-11-09 13:39:49 +0300

    Array of Objects

---
+++ Practice_Code-2.js
@@ -0,0 +1,5 @@
+const employees = [
+  { id: 101, name: "Sakshi Memon", email: "sakshi.memon@email.com" },
+  { id: 102, name: "Subhash Shukla", email: "subhash.shukla@email.com" },
+  { id: 103, name: "Mohini Karpe", email: "mohini.karpe@email.com" },
+];
