5f5ca342f3ccab5aca075be526af1820826cf7fd
Matthusala <88359082+Matthusala@users.noreply.github.com>
2023-11-25 22:18:11 +0300

    Update upvoteVsDownvote.js

--- upvoteVsDownvote.js
+++ upvoteVsDownvote.js
@@ -1,8 +1,6 @@
 function calculateNetVotes(votes) {
     return votes.upvotes - votes.downvotes;
 }
-
 const voteCounts = { upvotes: 10, downvotes: 5 };
 const netVotes = calculateNetVotes(voteCounts);
-
 console.log(`Net Votes: ${netVotes}`);
